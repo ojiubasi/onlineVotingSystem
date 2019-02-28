83565748ac1be674b131576673564ad68c5ea259
Ubasioji <ubasioji@gmail.com>
2019-02-28 14:27:13 +0100

    add frontend and backend pages

---
+++ assets/js/candidateReg.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {     
+    $("#candidateReg").on("click", function(e) {
+        e.preventDefault()
+        var firstName = $("#firstname")
+        var lastName = $("#lastname")
+        var office = $("#office")  
+        var email = $("#email")
+        var politicalParty = $("#party")
+        var description = $("#describe")
+        const CID = Math.floor(Math.random() * (1000 - 100 + 1)) + 100
+        var candidatesData = {
+            candidateId: CID,
+            firstName: firstName.val(),
+            lastName: lastName.val(),
+            office: office.val(),
+            email:      email.val(),
+            politicalParty:  politicalParty.val(),
+            description:  description.val() 
+        }
+        $.ajax({
+            method: "POST",
+            url: "http://localhost:3000/candidates",
+            data: candidatesData
+        })
+            .done(function (data) {
+                alert("voter registered successfully!")
+                window.location.assign("");
+            }).fail(function () {
+                alert("couldn't reach server")
+            })
+    })
+
+})
